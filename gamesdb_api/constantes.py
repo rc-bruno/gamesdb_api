758bb6705ed4d6fb95df7b22e7065d7aa005d575
Bruno Rodrigue <email@bruno.gs>
2023-02-23 21:12:53 -0300

    Corrigindo constante

--- gamesdb_api/constantes.py
+++ gamesdb_api/constantes.py
@@ -1,8 +1,8 @@
-from typing import Union, List
+from typing import Union, List, Dict
 URL_PESQUISA = 'https://thegamesdb.net/search.php?name=<search>&platform_id%5B%5D=<id>'
 URL_GAME_ID = 'https://thegamesdb.net/game.php?id=<id>'
 URL_CONSOLES = 'https://thegamesdb.net/list_platforms.php'
 URL_CONSOLE_ID = "https://thegamesdb.net/platform.php?id=<id>"
 URL_CONSOLE_GAMES = 'https://thegamesdb.net/list_games.php?platform_id=<id>&page=<page>'
-TYPE_GAME = dict[str, Union[str, Union[List[str], str]]]
+TYPE_GAME = Dict[str, Union[str, Union[List[str], str]]]
 
